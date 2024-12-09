3e0ae122797572c58d50dfaa469a1c13a02ef597
Bony Amin <bonyamincse@gmail.com>
2024-12-09 21:42:00 +0600

    Pattern1.cpp

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -4,7 +4,7 @@ int main(){
     int n;
     cout << "Enter a number: ";
     cin >> n;
-    for(int i = 0; i<=n; i++){
+    for(int i = 0; i<n; i++){
         for(int j = 1; j<=n; j++){
             cout << j << " ";
         }
